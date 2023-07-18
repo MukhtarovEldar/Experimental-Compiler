32b59f4aee0fe7c795c3b2f527e3bbe390859523
Eldar Mukhtarov <mukhtaroveldar05@gmail.com>
2023-07-19 00:22:53 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <cstdio>
 #include <cerrno>
 
-std::streampos file_size(std::FILE *file);
+long file_size(std::FILE *file);
 void print_usage(char **argv);
 char *file_contents(char *path);
 
@@ -13,7 +13,12 @@ int main(int argc, char **argv) {
         print_usage(argv);
         exit(0);
     }
-
+    char *path = argv[1];
+    char *contents = file_contents(path);
+    if(contents){
+        std::cout<<"Contents of "<<path<<":\n---\n"<<contents<<"\n---\n";
+        delete[] contents;
+    }
     return 0;
 }
 
@@ -21,28 +26,35 @@ void print_usage(char **argv) {
     std::cout << "USAGE: " << argv[0] << " <path_to_file_to_compile>";
 }
 
-char* file_contents(char *path) {
-    std::ifstream file(path);
-    if (!file.is_open()) {
+char *file_contents(char *path) {
+    std::FILE* file = std::fopen(path,"r");
+    if (!file) {
         std::cout << "Failed to open the file at " << path << '\n';
-        exit(1);
+        return nullptr;
     }
     long size = file_size(file);
     char *contents = new char[size + 1];
-    std::streamsize elements_read = file.readsome(contents, size);
-    if (elements_read != size) {
+    char *write_it = contents;
+    std::size_t bytes_read = 0;
+    while(bytes_read < size){
+        std::size_t bytes_read_this_iteration = std::fread(write_it, 1, size - bytes_read, file);
+        bytes_read += bytes_read_this_iteration;
+        write_it += bytes_read_this_iteration;
+    }
+    if (bytes_read < size) {
         delete[] contents;
         return nullptr;
     }
-    contents[size] = '\0'; 
+    contents[size] = '\0';
+    std::fclose(file); // remember to close
     return contents;
 }
 
-long file_size(std::ifstream& file) {
+long file_size(std::FILE *file) {
     if (!file) {
         return 0;
     }
-    std::streampos original = 0;
+    std::fpos_t original = 0;
     if (std::fgetpos(file, &original) != 0) {
         std::cout << "fgetpos() failed: " << errno << '\n';
         return 0;
