068b746488aa1365cd9bc8b37a4fe04f6971b739
Eldar Mukhtarov <mukhtaroveldar05@gmail.com>
2023-07-21 23:00:51 +0200

    Modified the code to pure C++

--- main.cpp
+++ main.cpp
@@ -1,71 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-#include <cstdio>
-#include <cerrno>
 
-long file_size(std::FILE *file);
-void print_usage(char **argv);
-char *file_contents(char *path);
+long fileSize(std::fstream &file);
+void displayUsage(char **argv);
+char *FileContents(char *path);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        print_usage(argv);
-        exit(0);
+        displayUsage(argv);
+        std::exit(0);
     }
     char *path = argv[1];
-    char *contents = file_contents(path);
-    if(contents){
-        std::cout<<"Contents of "<<path<<":\n---\n"<<contents<<"\n---\n";
+    char *contents = FileContents(path);
+    if (contents) {
+        std::cout << "Contents of " << path << ":\n---\n" << contents << "\n---\n";
         delete[] contents;
     }
     return 0;
 }
 
-void print_usage(char **argv) {
-    std::cout << "USAGE: " << argv[0] << " <path_to_file_to_compile>";
+void displayUsage(char **argv) {
+    std::cout << "Usage: " << argv[0] << " <file_path>";
 }
 
-char *file_contents(char *path) {
-    std::FILE* file = std::fopen(path,"r");
-    if (!file) {
+char *FileContents(char *path) {
+    std::fstream file(path, std::ios::in);
+    if (!file.is_open()) {
         std::cout << "Failed to open the file at " << path << '\n';
-        return nullptr;
+        std::exit(0);
     }
-    long size = file_size(file);
+    long size = fileSize(file);
     char *contents = new char[size + 1];
     char *write_it = contents;
-    std::size_t bytes_read = 0;
-    while(bytes_read < size){
-        std::size_t bytes_read_this_iteration = std::fread(write_it, 1, size - bytes_read - 1, file);
-        if(std::ferror(file)){
-            std::cout<<"Error while reading: " << errno << '\n';
-            delete[] contents;
-            return nullptr;
-        }
-        bytes_read += bytes_read_this_iteration;
-        write_it += bytes_read_this_iteration;
-        if(std::feof(file))
-            break;
+    file.read(write_it, size);
+    if (!file.is_open()) {
+        std::cout << "Error while reading!";
+        delete[] contents;
+        std::exit(0);
     }
-    contents[bytes_read] = '\0';
-    std::fclose(file); // remember to close
+    contents[size] = '\0';
     return contents;
 }
 
-long file_size(std::FILE *file) {
-    if (!file) {
-        return 0;
+long fileSize(std::fstream &file) {
+    if (!file.is_open()) {
+        std::exit(0);
     }
-    std::fpos_t original = 0;
-    if (std::fgetpos(file, &original) != 0) {
-        std::cout << "fgetpos() failed: " << errno << '\n';
-        return 0;
-    }
-    std::fseek(file, 0, SEEK_END);
-    long out = std::ftell(file);
-    if (std::fsetpos(file, &original) != 0) {
-        std::cout << "fsetpos() failed: " << errno << '\n';
-    }
+    std::streampos original = file.tellg();
+    file.seekg(0, std::ios::end);
+    long out = file.tellg();
+    file.seekg(original);
     return out;
 }
