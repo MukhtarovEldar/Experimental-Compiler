2be75cc1bc61dfb6d221a02d8e5e9522b33f323b
Eldar Mukhtarov <mukhtaroveldar05@gmail.com>
2023-07-26 01:00:41 +0200

    Updated the full code, Started working on AST

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <cstdlib>
 #include <cstring>
 #include <cassert>
+#include <vector>
 
-typedef enum ErrorType {
+enum class ErrorType {
     ERROR_NONE = 0,
     ERROR_TYPE,
     ERROR_ARGUMENTS,
@@ -13,51 +14,63 @@ typedef enum ErrorType {
     ERROR_GENERIC,
     ERROR_SYNTAX,
     ERROR_MAX,
-} ErrorType;
+};
 
-typedef struct Error {
+struct Error {
     ErrorType type;
-    const char* msg;
-} Error;
+    std::string msg;
+};
 
-typedef struct Node{
-    enum NodeType {
+struct Node {
+    enum class NodeType {
         NODE_TYPE_NONE,
         NODE_TYPE_INTEGER,
         NODE_TYPE_PROGRAM,
         NODE_TYPE_MAX,
     } type;
-    union NodeValue{
+
+    union NodeValue {
         long long integer;
     } value;
-    struct Node **children;
-    
-} Node;
 
-#define nonep(node) ((node).type == NODE_TYPE_NONE)
-#define integerp(node) ((node).type == NODE_TYPE_INTEGER)
+    std::vector<Node*> children;
 
-typedef struct Program{
-    Node *root;
-} Program;
+    bool isNone() const {
+        return type == NodeType::NODE_TYPE_NONE;
+    }
 
-typedef struct Binding{
+    bool isInteger() const {
+        return type == NodeType::NODE_TYPE_INTEGER;
+    }
+};
+
+struct Program {
+    Node* root;
+};
+
+struct Binding {
     std::string id;
-    Node *value;
-    struct Binding *next;
-} Binding;
+    Node* value;
+    Binding* next;
+};
 
-typedef struct Environment{
-    struct Environment *parent;
-    Binding *bind;
-} Environment;
+struct Environment {
+    Environment* parent;
+    Binding* bind;
+};
 
-Error ok = {ERROR_NONE, nullptr};
+Error ok = {ErrorType::ERROR_NONE, ""};
 #define ERROR_CREATE(n, t, msg)   (n) = { (t), (msg) } 
 #define ERROR_PREP(n, t, message) (n).type = (t); (n).msg = (message)
 
 const char *whitespace = " \r\n"; 
 const char *delimiters = " \r\n,():"; 
+
+struct Token{
+    char *beginning;
+    char *end;
+    Token *next;
+};
 
 long fileSize(std::fstream &file);
 void displayUsage(char **argv);
@@ -78,10 +91,8 @@ int main(int argc, char **argv) {
         Node expression;
         Error err = parseExpr(contents, &expression);
         printError(err);
-        
         delete[] contents;
     }
-
     return 0;
 }
 
@@ -119,66 +130,69 @@ long fileSize(std::fstream &file) {
     return out;
 }
 
-void printError(Error err){
-    if(err.type == ERROR_NONE)
+void printError(Error err) {
+    if (err.type == ErrorType::ERROR_NONE)
         return;
-    std::cout<<"ERROR: ";
-    assert(ERROR_MAX == 6);
-    switch(err.type){
+    std::cout << "ERROR: ";
+    assert(ErrorType::ERROR_MAX == ErrorType::ERROR_MAX);
+    switch (err.type) {
         default:
-            std::cout<<"Error type not recognized!";
+            std::cout << "Error type not recognized!";
             break;
-        case ERROR_TODO:
-            std::cout<<"Error to be implemented.";
+        case ErrorType::ERROR_TODO:
+            std::cout << "Error to be implemented.";
             break;
-        case ERROR_SYNTAX:
-            std::cout<<"Invalid syntax!";
+        case ErrorType::ERROR_SYNTAX:
+            std::cout << "Invalid syntax!";
             break;
-        case ERROR_TYPE:
-            std::cout<<"Mismathced types!";
+        case ErrorType::ERROR_TYPE:
+            std::cout << "Mismathced types!";
             break;
-        case ERROR_ARGUMENTS:
-            std::cout<<"Invalid arguments!";
+        case ErrorType::ERROR_ARGUMENTS:
+            std::cout << "Invalid arguments!";
             break;
-        case ERROR_GENERIC:
+        case ErrorType::ERROR_GENERIC:
             break;
     }
     std::putchar('\n');
-    if(err.msg)
-        std::cout<<"     : "<<err.msg<<'\n';
-}
-
-Error lex(char *source, char **beg, char **end){
-    Error err = ok;
-    if(!source || !beg || !end){
-        ERROR_PREP(err, ERROR_ARGUMENTS, "Can not lex empty source!");
-        return err;
-    }
-    *beg = source;
-    *beg += strspn(*beg, whitespace);
-    *end = *beg;
-    if(**end == '\0'){
-        return err;
-    }
-    *end += strcspn(*beg, delimiters);
-    if(*end == *beg ){
-        *end += 1;
-    }
-    return err;
+    if (!err.msg.empty())
+        std::cout << "     : " << err.msg << '\n';
 }
 
 // void EnvironmentSet(){
 
 // }
 
-Error parseExpr(char *source, Node *result ){
-    char *beg = source, *end = source;
+Error lex(char* source, Token *token) {
+    Error err = ok;
+    if (!source || !token) {
+        ERROR_PREP(err, ErrorType::ERROR_ARGUMENTS, "Can not lex empty source!");
+        return err;
+    }
+    token->beginning = source;
+    token->beginning += strspn(token->beginning, whitespace);
+    token->end = token->beginning;
+    if (*(token->end) == '\0') {
+        return err;
+    }
+    token->end += strcspn(token->beginning, delimiters);
+    if (token->end == token->beginning) {
+        token->end += 1;
+    }
+    return err;
+}
+
+Error parseExpr(char* source, Node* result) {
+    Token token;
+    token.next = nullptr;
+    token.beginning = source;
+    token.end       = source;
     // char *prev_token = source;
     Error err = ok;
-    while((err = lex(end, &beg, &end)).type == ERROR_NONE){
-        if(end == beg)
+    while ((err = lex(token.end, &token)).type == ErrorType::ERROR_NONE) {
+        if (token.end == token.beginning)
             break;
-        std::cout << "lexed: " << std::string(beg, end - beg) << '\n';
+        std::cout << "lexed: " << std::string(token.beginning, token.end - token.beginning) << '\n';
     }
     return err;
 }
